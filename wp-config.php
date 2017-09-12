767f11a1a61b78a91cbd6099bdb199da4449b325
Sven van Hal <sven@svenvanhal.nl>
2017-09-12 18:54:28 +0200

    Debugging ON.

--- wp-config.php
+++ wp-config.php
@@ -4,12 +4,12 @@ define( 'WP_MEMORY_LIMIT', '160M' );
 define( 'WP_MAX_MEMORY_LIMIT', '256M' );
 define( 'WP_POST_REVISIONS', 5 );
 
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_LOG', false );
-define( 'WP_DEBUG_DISPLAY', false );
+define( 'WP_DEBUG_DISPLAY', true );
 
-define( 'MYSQL_CLIENT_FLAGS', MYSQL_CLIENT_SSL );
-define( 'MYSQL_SSL_CA', "/usr/local/share/ca-certificates/wisvch.crt" );
+// define( 'MYSQL_CLIENT_FLAGS', MYSQL_CLIENT_SSL );
+// define( 'MYSQL_SSL_CA', "/usr/local/share/ca-certificates/wisvch.crt" );
 
 if(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'){
   $_SERVER['HTTPS'] = 'on';
